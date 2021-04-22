666d3724b6ea775e3bc1c3d7575a0cad51bee5ed
Gustavo Morais <gustavohenriquecfal@gmail.com>
2021-04-22 16:15:19 -0300

    push

---
+++ pages/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+    
+</template>
+
+<script>
+
+
+export default {
+   data(){
+  return {
+        email: null,
+        senha: null
+    }
+   },
+    methods: {
+        async login(){
+            try {
+                await this.$auth.loginWith("local",{
+                    data: {
+                        email: this.email,
+                        senha: this.senha
+                    }
+                });
+                this.$router.push("/");
+            } catch (e){
+                console.log(e);
+            }
+        }
+    }
+
+}
+</script>
